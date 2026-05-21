91eb8a3ba339c1e555cdb981c47cc39eb62b88bb
jojogrg <fayekguirguis362@gmail.com>
2026-05-21 14:53:15 +0200

    added objects and  methods

---
+++ JavaGame_TicTacToe/src/main/java/Board.java
@@ -0,0 +1,27 @@
+public class Board{
+    private char[][] cells;
+
+    public Board(){
+
+    }
+
+    public boolean isCellEmpty(int x, int y){
+        return true;
+    }
+
+    public void place(int x, int y, char marker){
+
+    }
+
+    public boolean isFull(){
+        return true;
+    }
+
+    public void clear(){
+
+    }
+
+    public void print(){
+
+    }
+}
